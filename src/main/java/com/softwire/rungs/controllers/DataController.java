26fc4b9e328abbf553d65eb8134e585c8b52c2bb
Simon St John-Green <Simon.StJohn-Green@softwire.com>
2015-01-26 17:59:00 +0000

    Add POST endpoint

--- src/main/java/com/softwire/rungs/controllers/DataController.java
+++ src/main/java/com/softwire/rungs/controllers/DataController.java
@@ -3,12 +3,24 @@ package com.softwire.rungs.controllers;
 import com.softwire.rungs.models.StatisticsModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class DataController {
-    @RequestMapping("/data")
+    @RequestMapping(value="/data", method= RequestMethod.GET)
     public @ResponseBody StatisticsModel displayHome() {
+        StatisticsModel statisticsModel = new StatisticsModel();
+        statisticsModel.p1 = "Simon";
+        statisticsModel.p2 = "Rupert";
+        return statisticsModel;
+    }
+
+    @RequestMapping(value="/data", method= RequestMethod.POST)
+    public @ResponseBody StatisticsModel putData(){
+
+        //TODO How to get data in from request message body
+
         StatisticsModel statisticsModel = new StatisticsModel();
         statisticsModel.p1 = "Simon";
         statisticsModel.p2 = "Rupert";
